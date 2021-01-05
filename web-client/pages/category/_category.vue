1c38bf0c1cd9c0ea36f3367af80c4af1171495a4
0468386988 <yanxiaofeng2014bj@gmail.com>
2021-01-05 18:08:34 +1100

    difficulty page

---
+++ web-client/pages/category/_category.vue
@@ -0,0 +1,41 @@
+﻿<template>
+  <div class="d-flex mt-3 justify-center align-start">
+    <trick-list :tricks="tricks" class="mx-2" />
+
+    <v-sheet class="pa-3 mx-2 sticky" v-if="category">
+      <div class="text-h6">{{ category.name }}</div>
+      <v-divider class="my-1"></v-divider>
+      <div class="text-body-2">{{ category.description }}</div>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import TrickList from "../../components/trick-list";
+export default {
+  components: {TrickList},
+  data: () => ({
+    category: null,
+    tricks: [],
+  }),
+  computed: mapGetters('tricks', ['categoryById']),
+  async fetch() {
+    const categoryId = this.$route.params.category;
+    this.category = this.categoryById(categoryId)
+    this.tricks = await this.$axios.$get(`/api/categories/${categoryId}/tricks`)
+  },
+  head() {
+    if (!this.category) return {}
+    return {
+      title: this.category.name,
+      meta: [
+        {hid: 'description', name: 'description', content: this.category.description}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- web-client/pages/trick/_trick.vue
+++ web-client/pages/trick/_trick.vue
@@ -12,78 +12,78 @@
       </div>
     </div>
 
-    <div class="mx-2 sticky">
-      <v-sheet class="pa-3 mt-2">
-        <div class="text-h6">{{ trick.name }}</div>
-        <v-divider class="my-1"></v-divider>
-        <div class="text-body-2">{{ trick.description }}</div>
-        <div class="text-body-2">{{ trick.difficulty }}</div>
-        <v-divider class="my-1"></v-divider>
-        <div v-for="rd in relatedData" v-if="rd.data.length > 0">
-          <div class="text-subtitle-1">{{rd.title}}</div>
-          <v-chip-group>
-            <v-chip v-for="d in rd.data" :key="rd.idFactory(d)" small :to="rd.routeFactory(d)">
-              {{d.name}}
-            </v-chip>
-          </v-chip-group>
-        </div>
-      </v-sheet>
-    </div>
+    <v-sheet class="pa-3 ma-2 sticky">
+      <div class="text-h5">
+        <span>{{ trick.name }}</span>
+        <v-chip class="mb-1 ml-2" small :to="`/difficulty/${difficulty.id}`">
+          {{ difficulty.name }} Difficulty
+        </v-chip>
+      </div>
+      <v-divider class="my-1"></v-divider>
+      <div class="text-body-2">{{ trick.description }}</div>
+      <v-divider class="my-1"></v-divider>
+      <div v-for="rd in relatedData" v-if="rd.data.length > 0">
+        <div class="text-subtitle-1">{{rd.title}}</div>
+        <v-chip-group>
+          <v-chip v-for="d in rd.data" :key="rd.idFactory(d)" small :to="rd.routeFactory(d)">
+            {{d.name}}
+          </v-chip>
+        </v-chip-group>
+      </div>
+    </v-sheet>
   </div>
 </template>
 
 <script>
 import {mapState, mapGetters} from 'vuex';
 export default {
+  data: () => ({
+    trick: null,
+    difficulty: null
+  }),
   computed: {
     ...mapState('submissions', ['submissions']),
     ...mapState('tricks', ['categories', 'tricks']),
-    ...mapGetters('tricks', ['trickById']),
-    trick() {
-      return this.trickById(this.$route.params.trick)
-    },
+    ...mapGetters('tricks', ['trickById', 'difficultyById']),
     relatedData() {
       return [
         {
           title: "Categories",
           data: this.categories.filter(x => this.trick.categories.indexOf(x.id) >= 0),
           idFactory: c => `category-${c.id}`,
-          routeFactory: c => `/`,
+          routeFactory: c => `/category/${c.id}`,
         },
         {
           title: "Prerequisites",
           data: this.tricks.filter(x => this.trick.prerequisites.indexOf(x.id) >= 0),
           idFactory: t => `trick-${t.id}`,
-          routeFactory: t => `/tricks/${t.id}`,
+          routeFactory: t => `/trick/${t.id}`,
         },
         {
           title: "Progressions",
           data: this.tricks.filter(x => this.trick.progressions.indexOf(x.id) >= 0),
           idFactory: t => `trick-${t.id}`,
-          routeFactory: t => `/tricks/${t.id}`,
+          routeFactory: t => `/trick/${t.id}`,
         },
       ]
     },
   },
   async fetch() {
     const trickId = this.$route.params.trick;
+    this.trick = this.trickById(this.$route.params.trick)
+    this.difficulty = this.difficultyById(this.trick.difficulty)
     await this.$store.dispatch("submissions/fetchSubmissionsForTrick", {trickId}, {root: true})
   },
-  head () {
+  head() {
+    if (!this.trick) return {}
     return {
       title: this.trick.name,
       meta: [
-        { hid: 'description', name: 'description', content: this.trick.description }
+        {hid: 'description', name: 'description', content: this.trick.description}
       ]
     }
   }
 }
 </script>
 
-<style scoped>
-.sticky {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 48px;
-}
-</style>
+<style scoped></style>
